3e7ec10d2ee3b2069be27dbe6dcf1a05a08a8e30
boonchoojit <jitnupong.b@ku.th>
2024-11-01 14:46:23 +0700

    edit task api

--- api/routes/task.js
+++ api/routes/task.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// add a new task
+router.post('/add', function(req, res, next) {
+
+});
+
+// get all tasks by priority
+router.get('/priority/:priority', function(req, res, next) {
+
+});
+
+// get all tasks
+router.get('/getAllTask', function(req, res, next) {
+
+});
+
+// update a task status by id 
 module.exports = router;
